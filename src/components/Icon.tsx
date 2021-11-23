4b99810ad199120416b68e8735012ed70ab36b86
mercurialVeracious1994 <mercurialVeracious@gmail.com>
2021-11-23 08:39:10 +0530

    [Garima] add icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,24 @@
+import { Image, StyleSheet, View } from 'react-native';
+import React from 'react';
+
+const Icon = () => {
+  return(
+    <View style={styles.container}>
+      <Image source={require('../../assets/icons/greenOval.png')} />
+      <Image style={styles.line} source={require('../../assets/icons/line.png')} />
+      <Image source={require('../../assets/icons/orangeOval.png')} />
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container:{
+    display: "flex",
+    flexDirection:"column",
+  },
+  line:{
+    left:5,
+    height:40
+  }
+})
+
+export default Icon;
