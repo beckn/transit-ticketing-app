99dcdf8bda59ee5521faab19802b105b87f96bc0
VarunDev <varundev76>
2021-11-23 23:07:14 +0530

    [Varun] Adding Routes Navigation + Partially (Login Page)

--- src/screens/auth/login.tsx
+++ src/screens/auth/login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { 
   StyleSheet,
   View,
@@ -7,10 +7,10 @@ import {
   Text,
   Dimensions
 } from "react-native";
-import BannerSvg from '../../../assets/svg/slides.svg';
+import BannerSvg from "../../../assets/svg/slides.svg";
 
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
 
 const LoginComponent = () => {
   return(
@@ -39,9 +39,9 @@ const LoginComponent = () => {
         <View style={[styles.buttonBox]}>
           <TouchableOpacity
             style={styles.loginButton}
-            onPress={() => console.log('Simple Button pressed')}
+            onPress={() => console.log("Simple Button pressed")}
           >
-            <Text style={{color: '#fff', fontSize: 18}}> Login </Text>
+            <Text style={{color: "#fff", fontSize: 18}}> Login </Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -59,18 +59,18 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   container:{
-    position: 'relative',
+    position: "relative",
     width: width / 1.1,
     height: height - 60
   },
   logoBox: {
-    position: 'relative',
+    position: "relative",
     top: 10,
     height: 40,
     marginBottom: 20
   },
   logoLabel: {
-    fontFamily: 'Inter-ExtraBold',
+    fontFamily: "Inter-ExtraBold",
     marginBottom: 20,
     fontWeight: "600",
     fontSize: 9
@@ -81,39 +81,39 @@ const styles = StyleSheet.create({
     marginLeft: 10
   },
   bannerTextBox: {
-    position: 'relative',
+    position: "relative",
     width: width ,
-    textAlign: 'left',
-    writingDirection: 'ltr',
+    textAlign: "left",
+    writingDirection: "ltr",
     marginTop: 20,
     marginBottom: 20,
   },
   bannerText: {
     fontWeight: "600",
     fontSize: 36,
-    fontFamily: 'Inter-ExtraBold',
+    fontFamily: "Inter-ExtraBold",
     includeFontPadding: false
   },
   bannerImageBox: {
     flex: 1,
-    flexDirection: 'column',
-		justifyContent: 'center', 
-		alignItems: 'center',
-    position: 'relative',
+    flexDirection: "column",
+		justifyContent: "center", 
+		alignItems: "center",
+    position: "relative",
     bottom: -50
   },
   inputTextBox: {
     marginTop: 100,
   },
   inputText: {
-    fontWeight: '100',
+    fontWeight: "100",
     fontSize: 22,
-    textAlign: 'left',
+    textAlign: "left",
     lineHeight: 27,
     marginVertical: 20,
   },
   inputTextForgot: {
-    fontWeight: '100',
+    fontWeight: "100",
     fontSize: 16,
     marginVertical: 20,
     lineHeight: 17
@@ -123,16 +123,16 @@ const styles = StyleSheet.create({
     width: width /1.1
   },
   loginButton: {
-    backgroundColor: '#3E4059',
+    backgroundColor: "#3E4059",
     borderRadius: 14,
     paddingVertical: 15,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   loginButtonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: '500',
+    fontWeight: "500",
     letterSpacing: 2,
   },
 });
