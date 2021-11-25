fc1fb63baecd9e101f0df15843807fbb82c2a8e9
mercurialVeracious1994 <mercurialVeracious@gmail.com>
2021-11-25 15:52:07 +0530

    [Garima] add chip component

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { colors } from "../../../assets/theme/colors";
+
+export const Chip: React.FC<{
+  label: string;
+}> = ({ label }): ReactElement => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderStyle: "solid",
+    borderRadius: 100,
+    backgroundColor: colors.Beige,
+    width: 83,
+    height: 36,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  label: {
+    color: colors.GreyBlack
+  }
+});
