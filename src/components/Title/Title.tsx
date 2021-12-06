1d6851c63bfe2c13dc6dff4fc75ce1d810d446ac
mercurialVeracious1994 <mercurialVeracious@gmail.com>
2021-12-06 17:12:34 +0530

    [Garima] add Title component

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,19 @@
+import React, { ReactElement } from "react";
+import { Text, StyleSheet } from "react-native";
+
+export const Title: React.FC<{ label: string }> = ({ label }): ReactElement => {
+  return (
+    <Text style={styles.loginPageTitle}>{label}</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  loginPageTitle: {
+    fontWeight: "200",
+    fontSize: 24,
+    lineHeight: 29,
+    fontFamily: "Inter",
+    textAlign: "center",
+    marginVertical: 20
+  }
+});
