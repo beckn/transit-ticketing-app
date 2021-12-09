5d751bc167e7843110797945725c5a51577e916d
mercurialVeracious1994 <mercurialVeracious@gmail.com>
2021-12-09 17:27:03 +0530

    [Garima] consume logo component in header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,49 +1,44 @@
 import React, { ReactElement } from "react";
-import { 
+import {
   StyleSheet,
   View,
   Text
 } from "react-native";
 import { NavigationScreenProp } from "react-navigation";
-import  LogOut from "../../../assets/svg/LogOut";
+import LogOut from "../../../assets/svg/LogOut";
 import Logo from "../../../assets/svg/Logo";
 const Header: React.FC<{
-  navigation?: NavigationScreenProp<any,any>    // eslint-disable-line @typescript-eslint/no-explicit-any
+  navigation?: NavigationScreenProp<any, any>    // eslint-disable-line @typescript-eslint/no-explicit-any
   showLogOutButton?: boolean,
 }> = ({ navigation, showLogOutButton }): ReactElement => {
   const logoLabel = "Government of Kerala";
-  return(
-    <View style={ styles.logoBox }>
+  return (
+    <View style={styles.logoBox}>
       <View>
         <Logo></Logo>
         <Text style={styles.logoLabel}>{logoLabel}</Text>
       </View>
-      {showLogOutButton && 
-          <View onTouchEndCapture={() => navigation?.navigate("LoginPage")} >
-            <LogOut style={styles.logoutIcon}/>
-          </View>
-      } 
+      {showLogOutButton &&
+        <View onTouchEndCapture={() => navigation?.navigate("LoginPage")} >
+          <LogOut />
+        </View>
+      }
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   logoBox: {
-    width:"100%",
+    width: "100%",
     height: 40,
     flexDirection: "row",
-    alignItems:"center",
+    alignItems: "center",
     justifyContent: "space-between"
   },
   logoLabel: {
     fontFamily: "Inter-ExtraBold",
     fontWeight: "600",
     fontSize: 9
-  },
-  logoutIcon: {
-    width: 35,
-    height: 35,
-    right: 40
   }
 });
 
