5f95dffdff3fb77430218aea2fe6f5410c527a9c
mercurialVeracious1994 <mercurialVeracious@gmail.com>
2021-12-20 21:16:19 +0530

    [Garima] refactor the Title  component to take style  as a prop

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,9 +1,10 @@
 import React, { ReactElement } from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, StyleProp, ViewStyle } from "react-native";
+import { colors } from "../../../assets/theme/colors";
 
-export const Title: React.FC<{ label: string }> = ({ label }): ReactElement => {
+export const Title: React.FC<{ label: string, style?: StyleProp<ViewStyle> }> = ({ label, style }): ReactElement => {
   return (
-    <Text style={styles.loginPageTitle}>{label}</Text>
+    <Text style={[ styles.loginPageTitle , style ]}>{label}</Text>
   );
 };
 
@@ -11,8 +12,10 @@ const styles = StyleSheet.create({
   loginPageTitle: {
     fontWeight: "200",
     fontSize: 24,
+    alignSelf:"stretch",
     lineHeight: 29,
-    textAlign: "center",
-    marginVertical: 40
+    marginVertical: 40,
+    color: colors.GreyBlack,
+    textAlign: "center"
   }
 });
